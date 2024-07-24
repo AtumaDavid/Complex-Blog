1895a2cb2614e65dc543b4ff38e46a9a7328d37a
Atuma David <atumadavid35@gmail.com>
2024-07-24 16:08:34 +0100

    user  and post model

---
+++ model/Post/Post.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+// schema
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title for your post is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Post description is required"],
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Post Category is required"],
+    },
+    numViews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    disLikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    //  user who created the post
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please Author is required"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Post image is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// compile the post model
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
